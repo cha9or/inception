4b9b42bb5eca157ed4c20f72d0f856d9771e6c64
Mohamed Amine Boulanouar <moboulan@e3r9p16.1337.ma>
2025-08-31 11:10:57 +0100

    wordpress

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -2,57 +2,44 @@
 set -e
 
 if [ ! -e /etc/.firstrun ]; then
-    sed -i "s/listen = 127.0.0.1/listen = 9000/g" /etc/php*/php-fpm.d/www.conf
-    sed -i "s/memory_limit = 128M/memory_limit = -1/" /etc/php*/php.ini
-
+    sed -i "s/listen = 127.0.0.1/listen = 9000/g"  /etc/php/8.2/fpm/pool.d/www.conf
     touch /etc/.firstrun
 fi
 
-
 if [ ! -e /etc/.firstmount ]; then
 
-    if [ -f /run/secrets/db_password ] && export MYSQL_PASSWORD=$(cat /run/secrets/db_password)
-    if [ -f /run/secrets/wp_password ] && export WORDPRESS_PASSWORD=$(cat /run/secrets/wp_password)
-    if [ -f /run/secrets/wp_admin_password ] && export WORDPRESS_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+    [ -f /run/secrets/db_password ] && export MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+    [ -f /run/secrets/wp_password ] && export WORDPRESS_PASSWORD=$(cat /run/secrets/wp_password)
+    [ -f /run/secrets/wp_admin_password ] && export WORDPRESS_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+
+    chown -R www-data:www-data /var/www/html
+    chmod -R 755 /var/www/html
 
     if [ ! -f /var/www/html/wp-config.php ]; then
 
         # download
-        if [ ! -d "/var/www/html/wp-content" ]; then
-            wp core download --allow-root || true
-        fi
+        [ ! -d "/var/www/html/wp-content" ] && wp core download --allow-root || true
 
-        # create database
         wp config create --allow-root \
-                        --dbhost=mariadb:3306 \
-                        --dbname="${MYSQL_DATABASE}" \
-                        --dbuser="${MYSQL_USER}" \
-                        --dbpass="${MYSQL_PASSWORD}"
-
-        # configure redis
-        # wp config set WP_REDIS_HOST redis
-        # wp config set WP_REDIS_PORT 6379 --raw
-        # wp config set WP_CACHE true --raw
-        # wp config set FS_METHOD direct
-
-        # install page
+            --dbhost=mariadb \
+            --dbuser="$MYSQL_USER" \
+            --dbpass="$MYSQL_PASSWORD" \
+            --dbname="$MYSQL_DATABASE"
+ 
         wp core install --allow-root \
-                        --skip-email \
-                        --url="${DOMAINE_NAME}" \
-                        --title="${WORDPRESS_TITLE}" \
-                        --admin_user="${WORDPRESS_ADMIN_USER}" \
-                        --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-                        --admin_email="${WORDPRESS_ADMIN_EMAIL}"
+            --skip-email \
+            --url="$DOMAIN_NAME" \
+            --title="$WORDPRESS_TITLE" \
+            --admin_user="$WORDPRESS_ADMIN_USER" \
+            --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+            --admin_email="$WORDPRESS_ADMIN_EMAIL"
 
-        if ! wp user get --allow-root "${WORDPRESS_USER}" >/dev/null 2>/dev/null  ;then
-            wp user create  --allow-root "${WORDPRESS_USER}" "${WORDPRESS_EMAIL}" --user_pass="${WORDPRESS_PASSWORD}" --role author
-        fi
-
+        wp user create "$WORDPRESS_USER" "$WORDPRESS_EMAIL" \
+            --user_pass="$WORDPRESS_PASSWORD" \
+            --role=author
     fi
 
-    chown -R www-data:www-data /var/www/html
-    chmod -R 755 /var/www/html
     touch /etc/.firstmount
 fi
 
-exec php-fpm -F
+exec php-fpm8.2 -F
